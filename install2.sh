8ed198105ccee9415e2c6936ef3bf2033df270df
inetms-soc <csoc_inetms@outlook.com>
2022-12-13 22:52:55 +0700

    Changes Code

--- install2.sh
+++ install2.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 #Define System Variable
 mkdir /home/socadmin
 export soc_path="/home/socadmin"
@@ -8,31 +7,29 @@ cp -a ./VPN_Script/*.sh $soc_path
 echo "VPN Client To Site begin install..."
 sleep 1.2
 #echo -e "Installing PPP Package\n"
-read -p "Do you want to install ppp expect package y/n? " yn
-if [ $yn == "Y" || $yn == "y"  ]
-then
-   echo -e "Installing PPP Package\n"
-   apt install ppp expect -y
-   echo "PPP Package was installed"
-else
-fi
-   read -p "Do you want to install ssl vpn package y/n? " yn
-   if [ $yn == "Y" || $yn == "y"  ]
-   then
-      echo -e "Installing SSL VPN Package\n"
-      apt install ./VPN_Script/forticlient-sslvpn_4.4.2333-1_amd64.deb -y
-      echo -e "SSL VPN Package was installed\n"
-   else
-   fi
-      read -p "Do you want to install nxlog -ce package y/n? " yn
-      if [ $yn == "Y" || $yn == "y"  ]
-      then
-         echo -e "Installing SSL VPN Package\n"
-         apt install ./NXLOG-Agents/NXLog_Ubuntu_Agents/nxlog-ce_3.1.2319_ubuntu18_amd64.deb -y
-         echo -e "NXLOG-CE Package was installed\n"
-         cp -a ./NXLog_Config/nxlog_server.conf /etc/nxlog/nxlog.conf
-         echo "Copy NXLog Configuration Complete.....!"
-      else
-         echo "Nothing Change!!"     
+read -p "Do you want to install ppp expect package y/n? " x1
+read -p "Do you want to install ssl vpn package y/n? " x2
+read -p "Do you want to install nxlog ce package y/n? " x3
 
-      fi  
+if [$x1 == "Y" || $x1 == "y"  ]
+then
+  echo -e "Installing PPP Package\n"
+  apt install ppp expect -y
+  echo "PPP Package was installed"
+
+elif [$x2 == "Y" || $x2 == "y"  ]
+then
+  echo -e "Installing SSL VPN Package\n"
+  apt install ./VPN_Script/forticlient-sslvpn_4.4.2333-1_amd64.deb -y
+  echo -e "SSL VPN Package was installed\n"
+
+elif [$x3 == "Y" || $x3 == "y"  ]
+then
+  echo -e "Installing SSL VPN Package\n"
+  apt install ./NXLOG-Agents/NXLog_Ubuntu_Agents/nxlog-ce_3.1.2319_ubuntu18_amd64.deb -y
+  echo -e "NXLOG-CE Package was installed\n"
+  cp -a ./NXLog_Config/nxlog_server.conf /etc/nxlog/nxlog.conf
+  echo "Copy NXLog Configuration Complete.....!"
+else
+  echo "Not Install Anything"
+fi
