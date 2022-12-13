010aac0cf574c1aa3ee91c67ea046cf4dd506ff3
inetms-soc <csoc_inetms@outlook.com>
2022-12-13 17:31:38 +0700

    Installation Script

--- VPN_Script/install.sh
+++
@@ -1,7 +0,0 @@
-echo "VPN Client To Site begin install..."
-sleep 1.2
-echo "Installing PPP Package"
-apt install ppp expect -y
-echo "Installing Forti SSL-VPN"
-apt install ./forticlient-sslvpn_4.4.2333-1_amd64.deb -y
-echo "Install Forti SSL-VPN Done...!"
---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#Define System Variable
+mkdir /home/socadmin
+export soc_path = "/home/socadmin"
+cp -a ./VPN_Script/*.sh $soc_path
+
+
+
+echo "VPN Client To Site begin install..."
+sleep 1.2
+#echo -e "Installing PPP Package\n"
+while true; do
+    read -p "Do you want to install ppp expect package y/n? " yn
+    case $yn in
+        [Yy]* )
+        echo -e "Installing PPP Package\n"
+        apt install ppp expect -y
+        echo "PPP Package was installed"
+        break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+echo -e "Installing Forti SSL-VPN\n"
+apt install ./VPN_Script/forticlient-sslvpn_4.4.2333-1_amd64.deb -y
+echo "Install Forti SSL-VPN Complete.....!"
+
+echo -e "Installing NXLog-CE\n"
+apt install ./NXLOG-Agents/NXLog_Ubuntu_Agents/nxlog-ce_3.1.2319_ubuntu18_amd64.deb -y
+echo "Install NXLog-CE Complete.....!"
+echo "Copy NXLog Configuration Complete.....!"
+cp -a ./NXLog_Config/nxlog_server.conf /etc/nxlog/nxlog.conf
