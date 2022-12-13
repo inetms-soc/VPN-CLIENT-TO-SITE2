64122860e759f8d3f02ffb68df1cd868011c8a81
inetms-soc <csoc_inetms@outlook.com>
2022-12-13 15:36:45 +0700

    Change Install.sh

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Description
 This Configuration for implement log collector and send log over ssl-vpn to logrelay SOC Team
 
 Authors: James Supagit
+
+User: inetms-soc
+Private Key: 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
--- VPN_Script/install.sh
+++ VPN_Script/install.sh
@@ -1,7 +1,7 @@
 echo "VPN Client To Site begin install..."
-sleep 1
+sleep 1.2
 echo "Installing PPP Package"
-apt install ppp expect
+apt install ppp expect -y
 echo "Installing Forti SSL-VPN"
-apt install ./VPN-CLIENT-TO-SITE/VPN_Script/forticlient-sslvpn_4.4.2333-1_amd64.deb
-#echo "Installing Forti SSL-VPN"
+apt install ./VPN_Script/forticlient-sslvpn_4.4.2333-1_amd64.deb -y
+echo "Installing Forti SSL-VPN"
