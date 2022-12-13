7756fecbffd0b83071bb61987c740c22fde86eb3
inetms-soc <csoc_inetms@outlook.com>
2022-12-13 23:43:00 +0700

    update installation script

--- install.sh
+++ install.sh
@@ -5,8 +5,6 @@ mkdir /home/socadmin
 export soc_path="/home/socadmin"
 cp -a ./VPN_Script/*.sh $soc_path
 
-
-
 echo "VPN Client To Site begin install..."
 sleep 1.2
 #echo -e "Installing PPP Package\n"
@@ -19,18 +17,52 @@ while true; do
         echo "PPP Package was installed"
         break;;
         # if type no = exit
-        [Nn]* ) 
-        exit;;
-        * ) 
+        [Nn]* )
+        break;;
+        * )
         echo "Please answer yes or no.";;
     esac
+done
 
-echo -e "Installing Forti SSL-VPN\n"
-apt install ./VPN_Script/forticlient-sslvpn_4.4.2333-1_amd64.deb -y
-echo "Install Forti SSL-VPN Complete.....!"
 
-echo -e "Installing NXLog-CE\n"
-apt install ./NXLOG-Agents/NXLog_Ubuntu_Agents/nxlog-ce_3.1.2319_ubuntu18_amd64.deb -y
-echo "Install NXLog-CE Complete.....!"
-echo "Copy NXLog Configuration Complete.....!"
-cp -a ./NXLog_Config/nxlog_server.conf /etc/nxlog/nxlog.conf
+while true; do
+    read -p "Do you want to install SSL VPN package y/n? " yn
+    case $yn in
+        [Yy]* )
+        echo -e "Installing Forti SSL-VPN\n"
+        apt install ./VPN_Script/forticlient-sslvpn_4.4.2333-1_amd64.deb -y
+        echo "Install Forti SSL-VPN Complete.....!"
+        break;;
+        # if type no = exit
+        [Nn]* )
+        break;;
+        * )
+        echo "Please answer yes or no.";;
+    esac
+done
+
+
+while true; do
+    read -p "Do you want to install NXLog-CE package y/n? " yn
+    case $yn in
+        [Yy]* )
+        echo -e "Installing NXLog-CE Package\n"
+        apt install ./NXLOG-Agents/NXLog_Ubuntu_Agents/nxlog-ce_3.1.2319_ubuntu18_amd64.deb -y
+        echo "Install NXLog-CE Complete.....!"
+        cp -a ./NXLog_Config/nxlog_server.conf /etc/nxlog/nxlog.conf
+        echo "Copy NXLog Configuration Complete.....!"
+        break;;
+        # if type no = exit
+        [Nn]* )
+        break;;
+        * )
+        echo "Please answer yes or no.";;
+    esac
+done
+
+echo -e "Validate Installed Packages..."
+apt list --installed | grep -i nxlog
+apt list --installed | grep -i forti
+apt list --installed | grep -i ppp
+
+echo "Installation Complete"
