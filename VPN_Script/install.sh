e2372fb7188cb47c68807157ca0958c13ac899e5
inetms-soc <csoc_inetms@outlook.com>
2022-12-13 16:13:11 +0700

    change name

--- VPN_Script/install.sh
+++ VPN_Script/install.sh
@@ -3,5 +3,5 @@ sleep 1.2
 echo "Installing PPP Package"
 apt install ppp expect -y
 echo "Installing Forti SSL-VPN"
-apt install ./VPN_Script/forticlient-sslvpn_4.4.2333-1_amd64.deb -y
+apt install ./forticlient-sslvpn_4.4.2333-1_amd64.deb -y
 echo "Install Forti SSL-VPN Done...!"
