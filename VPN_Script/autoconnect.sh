9971bd1e4c35582a40bd085150aeb09cf49460e2
inetms-soc <csoc_inetms@outlook.com>
2023-01-26 19:03:54 +0700

    update vpn

--- VPN_Script/autoconnect.sh
+++ VPN_Script/autoconnect.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-ping -w 5 logrelay1.local > /dev/null 2>&1
+ping -w 5 logrelay2.local > /dev/null 2>&1
 if [[ $? -eq 0 ]]; then
     echo "Ping worked"
 else
     echo "Ping failed"
     sleep 2
-    /home/socadmin/forti-vpn.sh &
+    /home/user01/forti-vpn.sh &
     systemctl restart nxlog
     systemctl status nxlog
 fi
