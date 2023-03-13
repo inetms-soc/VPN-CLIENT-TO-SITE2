0d2b6e85dcfa29c3e023168ef12971c7e86b9117
inetms-soc <120172862+inetms-soc@users.noreply.github.com>
2023-03-13 15:15:56 +0700

    Create dependency.sh

---
+++ dependency.sh
@@ -0,0 +1,2 @@
+wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl1.0/libssl1.0.0_1.0.2n-1ubuntu5_amd64.deb
+wget http://archive.ubuntu.com/ubuntu/pool/main/p/perl/libperl5.26_5.26.1-6_amd64.deb
