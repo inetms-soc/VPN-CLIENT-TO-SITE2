4dc073cbd03792f18ea9fb2fe5595d2380fedfdb
inetms-soc <120172862+inetms-soc@users.noreply.github.com>
2023-03-13 15:20:46 +0700

    Download Dependency Packages for nxlog 2.10

--- dependency.sh
+++ dependency.sh
@@ -1,2 +1,4 @@
 wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl1.0/libssl1.0.0_1.0.2n-1ubuntu5_amd64.deb
 wget http://archive.ubuntu.com/ubuntu/pool/main/p/perl/libperl5.26_5.26.1-6_amd64.deb
+wget http://archive.ubuntu.com/ubuntu/pool/main/g/gdbm/libgdbm5_1.14.1-6_amd64.deb
+wget http://archive.ubuntu.com/ubuntu/pool/main/p/perl/perl-modules-5.26_5.26.1-6_all.deb
