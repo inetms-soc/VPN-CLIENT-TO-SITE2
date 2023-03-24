ab7ac4ca7c6af8a772060d06272de2a19161581a
inetms-soc <csoc_inetms@outlook.com>
2023-03-24 15:15:10 +0700

    Update Change DiskMonitor Onemail

--- VPN_Script/diskmonitor.sh
+++ VPN_Script/diskmonitor.sh
@@ -10,8 +10,8 @@ DISK_USAGE=$(df -h / | awk 'NR==2{print $5}' | cut -d'%' -f1)
 # Check if the disk usage exceeds the threshold
 if [ $DISK_USAGE -gt $THRESHOLD ]; then
     # If it does, send a notification to Line Alert
-    MESSAGE="Disk usage on *$(hostname)* IP: *$PRIV_IP*  is currently at *$DISK_USAGE%*"
+    MESSAGE="Disk usage on path /var *$(hostname)* IP: *$PRIV_IP*  is currently at *$DISK_USAGE%*"
     echo $MESSAGE
-    curl -X POST -H 'Authorization: Bearer mi6GxeQXqPxLATRvus3aj6WwBMq2x3lSA3KB1bAcsm0' -F "message=$MESSAGE" https://notify-api.line.me/api/notify
+    curl -X POST -H 'Authorization: Bearer RYn2w26pdowMm8ChW7WDKz7sKHHpBMoGQweAmlKfMeH' -F "message=$MESSAGE" https://notify-api.line.me/api/notify
 fi
 
